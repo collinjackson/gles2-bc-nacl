44473a70843df1ee52a6a6c695fc2b5ecdf8c5c8
Collin Jackson <jackson@mcawesomerson2.local>
2011-02-19 14:08:00 -0800

    Add OpenGLES headers for gles2-bc, stub out unavailable glext calls

---
+++ OpenGLES/ES2/stubs.c
@@ -0,0 +1,14 @@
+#include "gl.h"
+#include "glext.h"
+
+void* glMapBufferOES (GLenum target, GLenum access) {
+  // NOTIMPLEMENTED
+}
+
+GLboolean glUnmapBufferOES (GLenum target) {
+  // NOTIMPLEMENTED
+}
+
+void glGetBufferPointervOES (GLenum target, GLenum pname, GLvoid** params) {
+  // NOTIMPLEMENTED
+}
